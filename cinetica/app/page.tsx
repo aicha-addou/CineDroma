b976c401a9fa935384767d56b642280b14720ccb
aicha-addou <addouaicha2003@gmail.com>
2024-11-21 09:41:11 +0100

    add : page de connexion

--- cinetica/app/page.tsx
+++ cinetica/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 import { useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";  // Importation de useRouter
-
+import { useRouter } from "next/navigation";
 export default function Home() {
   // Définition des états
   const [username, setUsername] = useState(""); // État pour le nom d utilisateur
