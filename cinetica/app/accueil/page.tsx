72319cc051318e5ef3d8be01bcaef72c8016d83c
aicha-addou <addouaicha2003@gmail.com>
2024-11-21 08:54:37 +0100

    add : page de connexion

---
+++ cinetica/app/accueil/page.tsx
@@ -0,0 +1,11 @@
+// app/accueil.tsx
+
+export default function Home() {
+    return (
+      <div className="flex items-center justify-center min-h-screen text-center">
+        <h1 className="text-4xl font-bold">Bienvenue sur la page d'accueil !</h1>
+        <p className="mt-4 text-xl">To be continued...</p>
+      </div>
+    );
+  }
+  
---
+++ cinetica/repository/user.ts
@@ -0,0 +1,6 @@
+// repository/user.ts
+export const user = {
+    username: 'admin@gmail.com',
+    password: '$2y$10$TWT7P.vRyjfILbKLQYrHAu7AWc1jEwf45eNj7BqZd0D.vyMUTA.Uq' // Mot de passe haché
+  };
+  
