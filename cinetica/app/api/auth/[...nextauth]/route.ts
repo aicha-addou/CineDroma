acf7cb97096454972c0ff05541be2579dc228cd2
Ahmed-Benmansour <Benmansour3a1@gmail.com>
2024-12-14 18:28:44 +0100

    Modif ahmed 4

--- cinetica/app/api/auth/[...nextauth]/route.ts
+++ cinetica/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,29 @@
 import NextAuth from "next-auth"
-import { authOptions } from "@/lib"
+import CredentialsProvider from "next-auth/providers/credentials"
+import {user as crd} from "@/repository/user"
+import bcrypt from 'bcryptjs'
 
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Username + Password",
+            credentials : { username:{}, password:{}},
+            async authorize(credentials){
+                const pwdok = await bcrypt.compare(credentials?.password||"", crd.password)
+                if(crd.username === credentials?.username && pwdok){
+                    return{
+                        id: crd.username,
+                        cd: credentials
+                    }
+                }
+                return null
+            }
+        })
+    ],
+    pages:{
+        signIn: '/login'
+    },
+}
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
