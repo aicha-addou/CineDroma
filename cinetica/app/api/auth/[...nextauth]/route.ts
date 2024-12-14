ec9aff18d127d2ae7180056310e33ee8f1f388f2
Ahmed-Benmansour <Benmansour3a1@gmail.com>
2024-12-14 18:36:08 +0100

    Modif ahmed inshallah

--- cinetica/app/api/auth/[...nextauth]/route.ts
+++ cinetica/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,32 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import {user as crd} from "@/repository/user"
-import bcrypt from 'bcryptjs'
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { user as crd } from "@/repository/user";
 
-export const authOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Username + Password",
-            credentials : { username:{}, password:{}},
-            async authorize(credentials){
-                const pwdok = await bcrypt.compare(credentials?.password||"", crd.password)
-                if(crd.username === credentials?.username && pwdok){
-                    return{
-                        id: crd.username,
-                        cd: credentials
-                    }
-                }
-                return null
-            }
-        })
-    ],
-    pages:{
-        signIn: '/login'
-    },
-}
-const handler = NextAuth(authOptions)
+// Configuration NextAuth
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Username + Password",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const pwdok = await bcrypt.compare(credentials?.password || "", crd.password);
+        if (crd.username === credentials?.username && pwdok) {
+          return { id: crd.username, name: crd.username };
+        }
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+};
 
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+
+// Exportez uniquement les gestionnaires nécessaires
+export { handler as GET, handler as POST };
